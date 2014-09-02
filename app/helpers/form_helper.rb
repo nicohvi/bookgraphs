27a0118dccf2832a6262a6963a2e9f27fe86d3cd
Nicolay Hvidsten <nico.hvi@gmail.com>
2014-09-02 12:13:55 +0200

    stylish as fuck

--- app/helpers/form_helper.rb
+++ app/helpers/form_helper.rb
@@ -7,12 +7,25 @@ module FormHelper
 
   class LabelFormBuilder < ActionView::Helpers::FormBuilder
     include ActionView::Helpers::TagHelper
+    include ActionView::Helpers::FormTagHelper
     include ActionView::Context
 
     def input(attribute, options={})
       content_tag(:div, class: 'input') do
-        label(attribute) + text_field(attribute, options )
+        label(attribute) + text_field(attribute, options)
       end
+    end
+
+    def submit(*args)
+      options = args.extract_options!
+
+      content_tag(:button, type: 'submit') do
+        content_tag(:i, "", class: "fa fa-#{options[:icon]} fa-2x") +
+        content_tag(:span) do
+          options[:text]
+        end
+      end
+
     end
 
   end
